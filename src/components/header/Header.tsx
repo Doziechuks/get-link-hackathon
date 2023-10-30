eeab4a9d86fc50e700cacf4532debfa1226963fa
doziechuks <doziechuks1010@gmail.com>
2023-10-30 19:44:14 +0100

    update the navbar and hompage

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -25,6 +25,58 @@ const Header = () => {
             <div className={styles.title}>
               <h1>
                 <img src={light} alt="light bulb" className={styles.light} />
+                getlinked Tech Hackathon <span>1.0</span>{" "}
+                <div className={styles.titleImgBox}>
+                  <img src={link} alt="link" />
+                  <img src={spark} alt="spark" />
+                </div>
+              </h1>
+            </div>
+            <p>
+              Participate in getlinked tech Hackathon 2023 stand a chance to win
+              a Big prize
+            </p>
+            <CustomButton>
+              <Link to="/register">Register</Link>
+            </CustomButton>
+          </div>
+          <div className={styles.timerBox}>
+            <div>
+              00<span>H</span>
+            </div>
+            <div>
+              00<span>M</span>
+            </div>
+            <div>
+              00<span>S</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imgBox}>
+          <img src={man} alt="man" className={styles.man} />
+          <div className={styles.bobble}>
+            <img src={bobble} alt="bobble light" />
+          </div>
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
+
+{
+  /* <div className={styles.highlight}>
+        <span>Igniting a Revolution in HR Innovation</span>
+        <img src={curve} alt="curve icon" className={styles.desktop} />
+        <img src={mobileCurve} alt="curve icon" className={styles.mobile} />
+      </div>
+      <section className={styles.wrapper}>
+        <div className={styles.writeupBox}>
+          <div className={styles.headingTitle}>
+            <div className={styles.title}>
+              <h1>
+                <img src={light} alt="light bulb" className={styles.light} />
                 getlinked Tech Hackathon <span>1.0</span>
                 <div className={styles.titleImgBox}>
                   <img src={link} alt="link" />
@@ -58,25 +110,5 @@ const Header = () => {
             <img src={bobble} alt="bobble light" />
           </div>
         </div>
-      </section>
-    </header>
-  );
-};
-
-export default Header;
-
-{
-  /* <div className={styles.title1}>
-              <img src={light} alt="light bulb" />
-              <h1>getlinked Tech</h1>
-            </div>
-            <div className={styles.title2}>
-              <h1>
-                Hackathon <span>1.0</span>
-              </h1>
-              <div className={styles.titleImgBox}>
-                <img src={link} alt="link" />
-                <img src={spark} alt="spark" />
-              </div>
-            </div> */
+      </section> */
 }
